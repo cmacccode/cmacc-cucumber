3367cf4de6a71ed134d60be141bd704c094f684b
Willem Veelenturf <wveelenturf@bol.com>
2017-09-13 17:03:53 +0200

    run

--- bin/run.js
+++ bin/run.js
@@ -1,13 +1,16 @@
 #! /usr/bin/env node
+const path = require('path');
 const cucumber = require('cucumber');
 const cmaccCucumber = require('../index');
 
+//require('../index').call(cucumber);
 
-const cwd = process.cwd()
+const cwd = process.cwd();
+
 const cli = new cucumber.Cli({
   argv: process.argv,
   cwd,
   stdout: process.stdout
 });
 
- cli.run()
+cli.run()
